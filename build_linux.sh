2a69239feb9837cf0c6f0d47f458ba5bcb685bcd
Toni B <toni.blafield@peltonet.com>
2019-05-04 11:57:59 +0300

    Tuned build script

--- build_linux.sh
+++ build_linux.sh
@@ -1,2 +1,4 @@
 g++ -lcurl -Ofast cURLpp.cpp Easy.cpp Exception.cpp Form.cpp Info.cpp ip.cpp main.cpp Multi.cpp OptionBase.cpp Options.cpp internal/CurlHandle.cpp internal/OptionList.cpp internal/OptionSetter.cpp internal/SList.cpp  -o updater -I .
-cp -R skeleton/* .
+mkdir bin
+cp -R skeleton/* ./bin/
+mv updater ./bin/
